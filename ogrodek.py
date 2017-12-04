c40bf4bd7122796852ca14dc5d14215555b857cb
qwerty <fafafft@gmail.com>
2017-12-04 17:31:07 +0100

    dodanie opisu

--- ogrodek.py
+++ ogrodek.py
@@ -1,2 +1,9 @@
 class Ogrod:
-    pass
+    def __init__(self):
+        self.rozmiar = 4
+        self.rosliny = []
+    def opis(self):
+        return "Rozmiar={0},Rosliny={1}".format(self.rozmiar, self.rosliny)
+
+ogrod = Ogrod()
+print (ogrod.opis())
