77dd2c07432892be57f2f0042be41c3a225e82e6
JusDus <jusdus@wp.pl>
2017-12-05 23:25:55 +0100

    Klasa Roślina, metody i cechy. Marchewka

--- ogrodek.py
+++ ogrodek.py
@@ -2,8 +2,10 @@ class Ogrod:
     def __init__(self):
         self.rozmiar = 4
         self.rosliny = []
+
     def opis(self):
         return "Rozmiar={0},Rosliny={1}".format(self.rozmiar, self.rosliny)
 
 ogrod = Ogrod()
 print (ogrod.opis())
+
---
+++ roslina.py
@@ -0,0 +1,35 @@
+class Roslina:
+    def __init__(self, nazwa):
+        self.nazwa = nazwa
+        self.waga = 0
+        self.wilgotnosc = 0.0
+        self.wiek = 0
+        self.polozenie = 0
+
+    def rosnij(self, dni):
+        self.wiek += dni
+        self.waga += dni*2
+        self.wilgotnosc -= 0.1*self.wilgotnosc
+        if self.wiek > 100:
+            return "czas na zbiory"
+        elif self.wiek >= 70:
+            return "wkrotce zbiory"
+        elif self.wiek >= 40:
+            return "jeszcze dużo czasu"
+        else:
+            return "świeżo zasiane!"
+
+    def podlej(self, litry):
+        self.wilgotnosc += litry
+
+    def opis(self):
+        return "{} waży {} gram i ma {} dni. \n Wilgotność: {}\n Położenie: {}".format(self.nazwa, self.waga, self.wiek, self.wilgotnosc, self.polozenie)
+
+
+marchewka = Roslina("Marchewka")
+
+print(marchewka.rosnij(20))
+print(marchewka.opis())
+
+
+
